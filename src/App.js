c49482b5b26b78b09f98bc22eba0a4160f89fd31
victor-alegre <victoralegre@uni.pe>
2020-05-30 13:53:29 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState,useEffect} from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 
 function App() {
+
+  // state del formulario
+  const [busqueda, guardarBusqueda] = useState({
+    ciudad: '',
+    pais: ''
+  });
+  const [consultar, guardarConsultar] = useState(false);
+
+  const {ciudad, pais} = busqueda;
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      if(consultar){
+        const appId = '546406f4ce0ac78408987422190a56c5';
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`
+        
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+      
+        console.log(resultado);
+      }
+    }
+
+    consultarAPI();
+    
+  },[consultar])
+
   return (
     <Fragment>
       <Header 
@@ -13,7 +40,11 @@ function App() {
         <div className="container">
           <div className="row">
             <div className="col m6 s12">
-              <Formulario/>
+              <Formulario
+                busqueda={busqueda}
+                guardarBusqueda={guardarBusqueda}
+                guardarConsultar={guardarConsultar}
+              />
             </div>
             <div className="col m6 s12">
               2
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Formulario = () => {
+const Formulario = ({busqueda, guardarBusqueda, guardarConsultar}) => {
+    
+    const [error, guardarError] = useState(false);
+
+    //extraer ciudad y pais
+    const {ciudad, pais} = busqueda;
+    
+    // funcion que coloca los elementos en el state
+    const handleChange = e=> {
+        //actualizar eñ state
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name] : e.target.value
+        })
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        //validar
+        if(ciudad.trim() === '' || pais.trim() === ''){
+            guardarError(true);
+            return;
+        }
+        guardarError(false);
+        // Pasar a componente principal
+        guardarConsultar(true);
+    }
+
     return (
-        <form>
+        <form
+            onSubmit={handleSubmit}
+        >
+            {error ? <p className="red darken-4 error">Todos los campos son obligatorios</p>
+            : null}
             <div className="input-field col s12">
                 <input
                     type="text"
                     name="ciudad"
                     id="ciudad"
+                    value={ciudad}
+                    onChange={handleChange}
                 />
                 <label htmlFor="ciudad">Ciudad: </label>
             </div>
@@ -15,6 +49,9 @@ const Formulario = () => {
             <div className="input-field col s12">
                 <select
                     name="pais"
+                    id="pais"
+                    value={pais}
+                    onChange={handleChange}
                 >
                     <option value="">-- Seleccione país --</option>
                     <option value="US">Estados Unidos</option>
@@ -25,6 +62,14 @@ const Formulario = () => {
                     <option value="ES">España</option>
                     <option value="PE">Perú</option>
                 </select>
+            </div>
+
+            <div className="input-field col s12">
+                <input
+                    type="submit"
+                    value="Buscar Clima"
+                    className="waves-effect waves-light btn-large btn-block yellow accent-4"
+                />
             </div>
         </form>
     );
